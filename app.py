4cfd2a2ace2fec1317b29d35157e29568f480333
Chamoda De Silva <78342002+ChamodaDSilva@users.noreply.github.com>
2022-03-19 08:15:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,45 @@
-
 from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
 import joblib
+#import pickle
 
 app = Flask(__name__)
-
 model = joblib.load('concrete-predicter.joblib')
 
+# model = pickle.load(open('concrete-predicter.pkl','rb'))
 
-@app.route('/', methods=['POST', 'GET'])
+# @app.route('/')
+# def home():
+#     return "hello World"
+#
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     inputs=request.form.get('inputs')
+#     newDataArr = inputs.split()
+#     data = [[float(newDataArr[0]), float(newDataArr[1]), float(newDataArr[2]), float(newDataArr[3]), float(newDataArr[4]),float(newDataArr[5]), float(newDataArr[6]), float(newDataArr[7])]]
+#     prediction = np.array2string(model.predict(data)[0])
+#     print(prediction)
+#
+#     return jsonify({'result':str(prediction)})
+@app.route('/', methods=['POST'])
 def feedModel():
+
     print("backend started")
-    newdata = request.get_json('input')
+
+    newdata=request.form.get('inputs')
     print(newdata)
-    newDataArr = newdata['input'].split()
+    newDataArr = newdata.split()
     data = [
         [float(newDataArr[0]), float(newDataArr[1]), float(newDataArr[2]), float(newDataArr[3]), float(newDataArr[4]),
          float(newDataArr[5]), float(newDataArr[6]), float(newDataArr[7])]]
     prediction = np.array2string(model.predict(data)[0])
     print(prediction)
+    return jsonify({"output":str(prediction)})
+if __name__=='__main__':
+    app.run(debug=True)
 
-    return jsonify(prediction)
 
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)  # change to '0.0.0.0' your IPv4 address
+#
+#
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)  # change to '0.0.0.0' your IPv4 address
